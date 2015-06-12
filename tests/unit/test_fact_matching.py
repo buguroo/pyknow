8ab6caf27e56bd2810c964f9d6b736ec6c05ed6e
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2015-06-12 13:30:46 +0200

    DEFINED and UNDEFINED wildcards.

--- tests/unit/test_fact_matching.py
+++ tests/unit/test_fact_matching.py
@@ -1,10 +1,10 @@
 import pytest
-from hypothesis import given
+from hypothesis import given, assume
 from hypothesis import strategies as st
 
 from pyknow.fact import Fact
 
-from conftest import random_kwargs
+from conftest import random_kwargs, random_types
 
 
 def test_same_fact_contain_each_other():
@@ -47,3 +47,86 @@ def test_match_if_all_defined_is_present(kwargs):
 
     assert f0 in f1
     assert f1 not in f0
+
+
+def test_FactState_exists():
+    from pyknow import fact 
+
+    assert hasattr(fact, 'FactState')
+
+
+def test_FactState_is_Enum():
+    from pyknow.fact import FactState
+    import enum
+
+    assert issubclass(FactState, enum.Enum)
+
+
+def test_FactState_DEFINED():
+    from pyknow.fact import FactState
+
+    assert hasattr(FactState, 'DEFINED')
+
+
+def test_FactState_UNDEFINED():
+    from pyknow.fact import FactState
+
+    assert hasattr(FactState, 'UNDEFINED')
+
+
+@given(value=random_types)
+def test_match_with_FactState_DEFINED_True(value):
+    try:
+        hash(value)
+    except TypeError:
+        assume(False)
+
+    from pyknow.fact import FactState as fs
+    from pyknow.fact import Fact
+
+    f0 = Fact(something=value)
+    f1 = Fact(something=fs.DEFINED)
+
+    assert f0 in f1
+
+
+@given(kwargs=random_kwargs)
+def test_match_with_FactState_DEFINED_False(kwargs):
+    assume(not 'something' in kwargs)
+
+    from pyknow.fact import FactState as fs
+    from pyknow.fact import Fact
+
+    f0 = Fact(**kwargs)
+    f1 = Fact(something=fs.DEFINED)
+
+    assert f0 not in f1
+
+
+@given(kwargs=random_kwargs)
+def test_match_with_FactState_UNDEFINED_True(kwargs):
+    assume(not 'something' in kwargs)
+
+    from pyknow.fact import FactState as fs
+    from pyknow.fact import Fact
+
+    f0 = Fact(**kwargs)
+    f1 = Fact(something=fs.UNDEFINED)
+
+    assert f0 in f1
+
+
+@given(value=random_types)
+def test_match_with_FactState_UNDEFINED_False(value):
+    try:
+        hash(value)
+    except TypeError:
+        assume(False)
+
+    from pyknow.fact import FactState as fs
+    from pyknow.fact import Fact
+
+    f0 = Fact(something=value)
+    f1 = Fact(something=fs.UNDEFINED)
+
+    assert f0 not in f1
