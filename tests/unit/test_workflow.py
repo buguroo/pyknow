3fc7d22f4205b0fc00a24c9a8fd86c0309e8cb64
David Francos Cuartero <dfrancos@buguroo.com>
2016-04-25 15:24:14 +0200

    Test that AND is the default operator

--- tests/unit/test_workflow.py
+++ tests/unit/test_workflow.py
@@ -23,7 +23,7 @@ def test_rules_are_executed_once(to_declare_random):
         def rule2(self):
             nonlocal executions
             executions.append('rule2')
-        
+
     ke = Test()
     ke.reset()
 
@@ -32,9 +32,64 @@ def test_rules_are_executed_once(to_declare_random):
 
     for i in to_declare:
         ke.declare(Fact(something=i))
-    
+
+    for activation in ke.agenda.activations:
+        print(activation.facts)
+
     assert len(ke.agenda.activations) == 2
     ke.run()
+
+    assert executions.count('rule1') == 1
+    assert executions.count('rule2') == 1
+
+
+def test_default_is_and():
+    """
+        Test that AND is the default operator
+    """
+    from collections import defaultdict
+    from pyknow.engine import KnowledgeEngine
+    from pyknow.rule import Rule
+    from pyknow.fact import Fact
+
+    executions = []
+
+    class Test(KnowledgeEngine):
+        """ Test KE """
+        @Rule(Fact(something=1),
+              Fact(something=2))
+        def rule1(self):
+            """ First rule, something=1 and something=2"""
+            nonlocal executions
+            executions.append('rule1')
+
+        @Rule(Fact(something=3))
+        def rule2(self):
+            """ Second rule, only something=3 """
+            nonlocal executions
+            executions.append('rule2')
+
+    ke_ = Test()
+    ke_.reset()
+
+    to_declare = []
+
+    for i in range(1, 10):
+        to_declare.append(i)
+
+    to_declare = dict(enumerate(to_declare))
+
+    for k, n in to_declare.items():
+        ke_.declare(Fact(something=n))
+
+    results = defaultdict(list)
+    for activation in ke_.agenda.activations:
+        results[''.join([str(to_declare[a - 1])
+                         for a in activation.facts])].append(1)
+
+    assert dict(results) == {'3': [1], '12': [1]}
+    assert len(ke_.agenda.activations) == 2
+    ke_.run()
 
     assert executions.count('rule1') == 1
     assert executions.count('rule2') == 1
