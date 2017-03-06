9cf8356755be096f37bb0c2822ee5119de74cf79
David Francos Cuartero <dfrancos@buguroo.com>
2017-03-06 21:32:46 +0100

    Improvements in alpha network generation.
    Added ConflictSetNode in the end, small refactor on node extraction
    and added more documentation on callables

--- pyknow/rete/alpha.py
+++ pyknow/rete/alpha.py
@@ -5,10 +5,11 @@ from contextlib import suppress
 from operator import itemgetter
 
 from pyknow.engine import KnowledgeEngine
-from pyknow.fact import W, InitialFact
+from pyknow.fact import W, Fact
 from pyknow.rete import PRIORITIES
 from pyknow.rete.callables import Callables
 from pyknow.rete.nodes import FeatureTesterNode, OrdinaryMatchNode
+from pyknow.rete.nodes import ConflictSetNode
 from pyknow.rule import Rule, OR, NOT
 
 FIRST, SECOND, THIRD = PRIORITIES
@@ -71,7 +72,12 @@ class EngineWalker:
         - Its keys are a subset of the other fact's keys
           Only if it does not contain any wildcard.
         - Has its key (if not W mode)
-        - Specific condition.
+        - Specific condition depending on the fact content.
+          - T -> Calls a given callable
+          - None (Default) -> Direct value comparision
+          - W -> W(True), the key should be in the fact,
+                 W(False) the key should not be in the fact.
+          - C -> Context comparision
 
         """
         yield Callables.same_class(fact), FIRST
@@ -83,17 +89,6 @@ class EngineWalker:
             if not isinstance(value.__class__, W):
                 yield Callables.has_key(key), SECOND  # NOQA
             yield Callables.get_callable(key, value), THIRD
-
-    def get_beta_node(self, parent, left, right):
-        """
-        Given a two-node condition, return a beta node for it.
-        """
-        left_node = self.resolve_network(left, parent)
-        right_node = self.resolve_network(right, parent)
-        beta_node = OrdinaryMatchNode(Callables.and_match)
-        left_node.add_child(beta_node, beta_node._activate_left)
-        right_node.add_child(beta_node, beta_node._activate_right)
-        return beta_node
 
     def get_alpha_branch(self, alpha_cls, fact):
         """
@@ -113,29 +108,41 @@ class EngineWalker:
             with suppress(IndexError):
                 # pylint: disable=protected-access
                 alpha_node.add_child(nodes[num+1], nodes[num+1]._activate)
-        return nodes
+        # Latest node in the list has no children, we return it
+        # so its children will be the next node (a beta node)
+        return nodes[-1]
 
-    def resolve_network(self, conds, parent):
+    def get_beta_node(self, left, right):
         """
-        Recursively resolve network
+        Given a two-node condition, return a beta node for it.
+        If needed, resolve its children.
         """
-        conds = EngineWalker.normalize_branch(conds, parent.__class__)
+        # pylint: disable=protected-access
+        left_node = self.get_node(left)
+        right_node = self.get_node(right)
+        beta_node = OrdinaryMatchNode(Callables.and_match)
+        left_node.add_child(beta_node, beta_node._activate_left)
+        right_node.add_child(beta_node, beta_node._activate_right)
+        return beta_node
 
-        for cond in conds:
-            if isinstance(cond, Rule) and not isinstance(cond, NOT):
-                # Return a match node with the two elements on the network
-                # This relies on the fact that the network is normalized
-                # as 2-element tuples and that a rule is iterable.
-                yield self.get_beta_node(cond, cond[0], cond[1])
-            elif isinstance(cond, NOT):
-                # yield NotNode(network(cond, cls))
-                raise NotImplementedError()
-            elif isinstance(cond, OR):
-                raise NotImplementedError()
-            else:
-                alpha = self.get_alpha_branch(FeatureTesterNode, cond)
-                # yield the output node to be used by later j
-                yield alpha[-1]
+    def get_node(self, cond):
+        """
+        Return a node for a specific condition
+        """
+        if isinstance(cond, Rule) and not isinstance(cond, NOT):
+            # Return a match node with the two elements on the network
+            # This relies on the fact that the network is normalized
+            # as 2-element tuples and that a rule is iterable.
+            return self.get_beta_node(cond[0], cond[1])
+        elif isinstance(cond, NOT):
+            # yield NotNode(network(cond, cls))
+            raise NotImplementedError()
+        elif isinstance(cond, OR):
+            raise NotImplementedError()
+        elif isinstance(cond, Fact):
+            return self.get_alpha_branch(FeatureTesterNode, cond)
+        else:
+            raise Exception()
 
     def get_network(self):
         """ Generate alpha network """
@@ -143,8 +150,11 @@ class EngineWalker:
 
         for rule in self.engine.get_rules():
             beta_node = OrdinaryMatchNode(Callables.and_match)
-            last_nodes = list(self.resolve_network(rule, rule))
+
+            conds = EngineWalker.normalize_branch(rule, rule.__class__)
+            last_nodes = [self.get_node(cond) for cond in conds]
             left_node = last_nodes.pop()
+
             if last_nodes:
                 right_node = last_nodes.pop()
             else:
@@ -152,4 +162,7 @@ class EngineWalker:
 
             left_node.add_child(beta_node, beta_node._activate_left)
             right_node.add_child(beta_node, beta_node._activate_right)
-            yield beta_node
+
+            conflict_set = ConflictSetNode(rule)
+            beta_node.add_child(conflict_set, conflict_set._activate)
+            yield conflict_set
