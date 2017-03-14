74bb24900a6b447c989e9898668ce1c28a77f28e
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2017-03-14 16:54:57 +0100

    Import shortcuts.

---
+++ pyknow/__init__.py
@@ -0,0 +1,6 @@
+from .fact import Fact, InitialFact
+from .rule import Rule, AND, OR, NOT
+
+from .rule import LiteralPCE as L
+from .rule import PredicatePCE as P
+from .rule import WildcardPCE as W
