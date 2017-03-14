8129fd86e60d5529204a09a526dd7d3a74de94a4
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2017-03-14 09:43:56 +0100

    Conditional Element rework.

--- pyknow/rule.py
+++ pyknow/rule.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 """
+Definitions of clips' Conditional Elements, except ``Pattern Conditional
+Element``.
 
-Definitions of clips' Conditional Elements, except
-``Pattern Conditional Element``.
-
-``Pattern CE`` defines direct matching against patterns, wich is a special
-case implemented in :mod:`pyknow.fact`.
+``Pattern CE`` defines direct matching against patterns, wich is a
+special case implemented in :mod:`pyknow.fact`.
 
 """
 from collections import UserList
@@ -15,123 +14,85 @@ from pyknow.fact import InitialFact
 from pyknow.watchers import RULE_WATCHER
 
 
-# class Rule:
-#     """
-#     Base ``CE``, all ``CE`` are to derive from this class.
-# 
-#     This class is used as a decorator, thus provoking __call__
-#     to be called twice:
-# 
-#     #. The first call is when the decorator is been created. At this
-#        point we assign the function decorated to ``self.__fn`` and
-#        return ``self`` to be called the second time.
-# 
-#     #. The second call is to execute the decorated function, se we
-#        pass all the arguments along.
-#     """
-# 
-#     def __init__(self, *conds, salience=0):
-#         if not conds:
-#             conds = (InitialFact(),)
-#         self.__fn = None
-#         self.parent = None
-#         self._conds = list(conds)
-#         for cond in conds:
-#             if isinstance(cond, Rule):
-#                 cond.parent = self
-#         self._curr = 0
-#         self.salience = salience
-#         RULE_WATCHER.debug("Initialized rule with conds %s", conds)
-# 
-#     # def __iter__(self):
-#     #     return self._conds
-# 
-#     def __getitem__(self, item):
-#         return self._conds[item]
-# 
-#     def __setitem__(self, item, value):
-#         self._conds[item] = value
-# 
-#     def copy(self):
-#         copy = self.__class__(self._conds, salience=self.salience)
-#         copy.__fn = self.__fn
-#         copy.parent = self.parent
-#         copy._curr = self._curr
-#         return copy
-# 
-#     def __len__(self):
-#         return len(self._conds)
-# 
-#     def __next__(self):
-#         self._curr += 1
-#         try:
-#             return self._conds[self._curr - 1]
-#         except IndexError:
-#             raise StopIteration()
-# 
-#     def __repr__(self):
-#         return "{}{}".format(self.__class__.__name__, tuple(self._conds))
-# 
-#     def __hash__(self):
-#         return hash(tuple(hash(a) for a in self._conds))
-# 
-#     def __call__(self, fst=None, *args, **kwargs):
-#         """
-#         Make method checks if it's the first call, and update wrapper.
-#         Othersise execute the RHS.
-#         """
-#         if self.__fn is None and fst is None:
-#             raise AttributeError("Mandatory function not provided.")
-# 
-#         if self.__fn is None and fst is not None:
-#             self.__fn = fst
-#             return update_wrapper(self, self.__fn)
-# 
-#         if 'activation' in kwargs:
-#             activation = kwargs.pop('activation')
-#             RULE_WATCHER.debug("Executing %s for rule %s, activation %s",
-#                                self.__fn.__name__, self, activation)
-#             if activation.context:
-#                 kwargs.update({k.value: v.value for k, v in
-#                                activation.context.items()})
-# 
-#         return self.__fn(*tuple(obj for obj in (fst,) if obj) + args, **kwargs)
-# 
-#     def __eq__(self, other):
-#         return self._conds == other._conds
+class ConditionalElement(tuple):
+    """Base Conditional Element"""
 
-
-class Rule(tuple):
     def __new__(cls, *args):
-        return super(Rule, cls).__new__(cls, args)
+        return super(ConditionalElement, cls).__new__(cls, args)
 
     def __repr__(self):
         return "%s%s" % (self.__class__.__name__, super().__repr__())
 
 
-class AND(Rule):
+class Rule(ConditionalElement):
+    """
+    Base ``CE``, all ``CE`` are to derive from this class.
+
+    This class is used as a decorator, thus provoking __call__
+    to be called twice:
+
+    #. The first call is when the decorator is been created. At this
+       point we assign the function decorated to ``self.__fn`` and
+       return ``self`` to be called the second time.
+
+    #. The second call is to execute the decorated function, se we
+       pass all the arguments along.
+    """
+
+    def __new__(cls, *args, salience=0):
+        obj = super(Rule, cls).__new__(cls, *args)
+
+        obj.__fn = None
+        obj.salience = salience
+
+        RULE_WATCHER.debug("Initialized rule : %r", obj)
+
+        return obj
+
+    def __call__(self, fst=None, *args, **kwargs):
+        """
+        Make method checks if it's the first call, and update wrapper.
+        Othersise execute the RHS.
+        """
+        if self.__fn is None and fst is None:
+            raise AttributeError("Mandatory function not provided.")
+
+        if self.__fn is None and fst is not None:
+            self.__fn = fst
+            return update_wrapper(self, self.__fn)
+        else:
+            RULE_WATCHER.debug("Executing %s for rule %s, with context %s",
+                               self.__fn.__name__, self, kwargs)
+
+            return self.__fn(self, **kwargs)
+
+
+class AND(ConditionalElement):
     """
     ``AND CE``
     ----------
 
     See (:ref:`conditional_and`) narrative documentation.
     """
+
     pass
 
 
-class OR(Rule):
+class OR(ConditionalElement):
     """
     ``Or CE``
     ---------
     See (:ref:`conditional_or`) narrative documentation
     """
+
     pass
 
 
-class NOT(Rule):
+class NOT(ConditionalElement):
     """
     ``NOT CE``
     ----------
     See (:ref:`conditional_not`) narrative documentation
     """
+
     pass
